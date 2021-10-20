b41eaddad3c48d961c21b4cc82a4855c318ce4a1
Snagrap <karlkarilaid04@gmail.com>
2021-10-20 16:54:11 +0300

    working ui and app.js

---
+++ UI.js
@@ -0,0 +1,41 @@
+class UI {
+	addBook(title, author, isbn) {
+		const tr = document.createElement('tr');
+		tr.className = 'collection-item';
+
+		const tdAuthor = document.createElement('td');
+		tdAuthor.appendChild(document.createTextNode(author));
+		tr.appendChild(tdAuthor);
+
+		const tdTitle = document.createElement('td');
+		tdTitle.appendChild(document.createTextNode(title));
+		tr.appendChild(tdTitle);
+
+		const tdISBN = document.createElement('td');
+		tdISBN.appendChild(document.createTextNode(isbn));
+		tr.appendChild(tdISBN);
+
+		const tdLink = document.createElement('td')
+		const link = document.createElement('a');
+		link.className = 'secondary-content';
+		link.appendChild(document.createTextNode('X'));
+		link.setAttribute('href', '#');
+		tdLink.appendChild(link)
+		tr.appendChild(tdLink);
+
+		const list = document.querySelector('tbody.table-list');
+		list.appendChild(tr);
+
+		const input1 = document.querySelector('#book-title');
+		input1.value = "";
+		const input2 = document.querySelector('#book-author');
+		input2.value = "";
+		const input3 = document.querySelector('#book-isbn');
+		input3.value = "";
+	}
+	deleteBook(book){
+		if(book.textContent == "X"){
+			book.parentElement.parentElement.remove();
+		}
+	}
+}
---
+++ app.js
@@ -0,0 +1,24 @@
+ui = new UI();
+ls = new LS();
+
+const form = document.querySelector('form')
+const bookTitleInput = document.querySelector('#book-title');
+const bookAuthorInput = document.querySelector('#book-author');
+const bookISBNInput = document.querySelector('#book-isbn');
+const tabel = document.querySelector("body");
+
+form.addEventListener('submit', addBook);
+tabel.addEventListener("click", deleteBook);
+
+function addBook(e) {
+	const bookTitle = bookTitleInput.value;
+	const bookAuthor = bookAuthorInput.value;
+	const bookISBN = bookISBNInput.value;
+	ui.addBook(bookTitle, bookAuthor, bookISBN);
+	console.log('clicked')
+	e.preventDefault();
+}
+function deleteBook(e){
+	let book = e.target.parentElement.firstChild;
+	ui.deleteBook(book);
+}
